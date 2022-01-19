61ca5e0ccefd69570a6e4e2048bfc451515f6ec3
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-20 00:35:47 +0530

    Update MessageHandler.ts

--- src/Handlers/MessageHandler.ts
+++ src/Handlers/MessageHandler.ts
@@ -114,7 +114,7 @@ export default class MessageHandler {
 				return void M.reply(`Only MODS are allowed to use this command.`);
 			}
 			if (command.config?.adminOnly && !M.sender.isAdmin)
-				return void M.reply(`This command is only meant for the group admins, Pabo!`);
+				return void M.reply(`This command is only meant for the group admins, Baka!`);
 			try {
 				await command.run(M, this.parseArgs(args));
 				if (command.config.baseXp) {
