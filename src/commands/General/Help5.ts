aeb308004c1d48742bc4a16a9d46ae37b6a6a48e
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-21 01:18:41 +0530

    Update Help5.ts

--- src/commands/General/Help5.ts
+++ src/commands/General/Help5.ts
@@ -12,42 +12,39 @@ export default class Command extends BaseCommand {
 			command: "help5",
 			description: "Displays the info",
 			category: "general",
-			usage: `${client.config.prefix}kacchan`,
+			usage: `${client.config.prefix}nino`,
 		        dm: true,
-                        aliases: ['h5','?5','well5','menu5']
+                        aliases: ['h5','?5','menu5']
 		});
 	}
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
-		const kacchan = 
+		const nino = 
 			"https://c.tenor.com/o7nRC7YIrFEAAAPo/rider-fate.mp4";
 		return void this.client.sendMessage(
 			M.from,
-			{ url: kacchan },
+			{ url: nino },
 			MessageType.video,
 			{
 				quoted: M.WAMessage,
 				mimetype: Mimetype.gif,
-				caption: `-🄼🄾🄳🄴🅁🄰🅃🄸🄾🄽 🍁
-				
-────────────────				
+				caption: `╔═.✾. ❝ [ 𝙈𝙀𝘿𝙄𝘼 ] ❞ ════
+🎼 ${this.client.config.prefix}ɪɢᴜꜱᴇʀ 
+🎼 ${this.client.config.prefix}ᴋᴀʀᴀᴏᴋᴇ 
+🎼 ${this.client.config.prefix}ʟʏʀɪᴄꜱ 
+🎼 ${this.client.config.prefix}ᴘʟᴀʏ 
+🎼 ${this.client.config.prefix}ꜱᴘᴏᴛɪꜰʏ 
+🎼 ${this.client.config.prefix}ʏᴛᴀᴜᴅɪᴏ 
+🎼 ${this.client.config.prefix}ʏᴛꜱᴇᴀʀᴄʜ 
+🎼 ${this.client.config.prefix}ʏᴛᴠɪᴅᴇᴏ 
+🎼 ${this.client.config.prefix}ɪɢ 
+🎼 ${this.client.config.prefix}ɪɢᴘᴏsᴛ
+🎼 ${this.client.config.prefix}ᴘɪɴᴛᴇʀᴇsᴛ
+╚══════════════════.✾.
 
-〽 ${this.client.config.prefix}𝙖𝙘𝙩𝙞𝙫𝙖𝙩𝙚 
-〽 ${this.client.config.prefix}𝙘𝙡𝙤𝙨𝙚 
-〽 ${this.client.config.prefix}𝙙𝙚𝙖𝙘𝙩𝙞𝙫𝙖𝙩𝙚 
-〽 ${this.client.config.prefix}𝙙𝙚𝙡𝙚𝙩𝙚 
-〽 ${this.client.config.prefix}𝙙𝙚𝙢𝙤𝙩𝙚 
-〽 ${this.client.config.prefix}𝙚𝙫𝙚𝙧𝙮𝙤𝙣𝙚 
-〽 ${this.client.config.prefix}𝙜𝙧𝙤𝙪𝙥𝙘𝙝𝙖𝙣𝙜𝙚 
-〽 ${this.client.config.prefix}𝙤𝙥𝙚𝙣 
-〽 ${this.client.config.prefix}𝙥𝙧𝙤𝙢𝙤𝙩𝙚 
-〽 ${this.client.config.prefix}𝙥𝙪𝙧𝙜𝙚 
-〽 ${this.client.config.prefix}𝙧𝙚𝙢𝙤𝙫𝙚 
-〽 ${this.client.config.prefix}𝙖𝙙𝙙
-〽 ${this.client.config.prefix}𝙧𝙚𝙫𝙤𝙠𝙚
-    
-    
-────────────────
+❅───✧ ✦ ✧───❅
+📬 ɴᴏᴛᴇ: ᴜꜱᴇ ${this.client.config.prefix}ʜᴇʟᴘ <ᴄᴏᴍᴍᴀɴᴅ_ɴᴀᴍᴇ> ᴛᴏ ᴠɪᴇᴡ ᴛʜᴇ ᴄᴏᴍᴍᴀɴᴅ ɪɴꜰᴏ.
+❅───✧ ✦ ✧───❅
 
  `,
 			}
