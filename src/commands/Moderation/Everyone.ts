29f6f239fc8a9f51a9a1d3a19999fb2f83f6ff05
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-20 12:55:45 +0530

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -20,34 +20,23 @@ export default class Command extends BaseCommand {
 		});
 	}
 
-	run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
+	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const term = joined.trim();
-		const images = [		
-			"https://wallpapercave.com/wp/wp3144753.jpg",
-			"https://wallpapercave.com/wp/wp4782018.jpg",
-			"https://wallpaperaccess.com/full/1326836.jpg",
-			"https://wallpapermemory.com/uploads/711/chitoge-kirisaki-wallpaper-full-hd-323316.jpg",
-			"https://data.whicdn.com/images/304776416/original.jpg",
-			"https://i.pinimg.com/564x/ca/e7/8a/cae78ad7f8e6459ad20bde350e2eb78b.jpg",
-		];
-		const selected = images[Math.floor(Math.random() * images.length)];
-		const term = joined.trim().split(" ");
-			const images: any = await new Image(random, {
-				pack: "READ QUOTED MESSAGE",
-				author: "Nino",
-				quality: 90,
-				type: "default",
-				categories: ["🎊"],
-			});
-			return void (await M.reply( { url: selected }, MessageType.jpg));
-				MessageType.image,
-				mimetype: Mimetype.jpg,
+		const nino =		
+			"https://c.tenor.com/XVLRX-3bx6MAAAPo/lisa-cute.mp4",
+			
+		return void this.client.sendMessage(
+			M.from,
+			{ url: nino },
+			MessageType.video,
+			{
+				quoted: M.WAMessage,
+				mimetype: Mimetype.gif,
 				M.groupMetadata?.participants.map((user) => user.jid),
 				},
-			});
+		        }
+		);
+		      
 		} else
 			return void (await M.reply(
 				`${
