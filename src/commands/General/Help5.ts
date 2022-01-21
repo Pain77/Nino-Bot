f86ffc345c574420d8f49d83ae21df0cdc368ac1
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-21 07:09:15 +0000

    Update Help5.ts

--- src/commands/General/Help5.ts
+++ src/commands/General/Help5.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const nino = 
-			"https://c.tenor.com/o7nRC7YIrFEAAAPo/rider-fate.mp4";
+			"https://c.tenor.com/ozcM_57dbnsAAAPo/nino-nakano.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: nino },
