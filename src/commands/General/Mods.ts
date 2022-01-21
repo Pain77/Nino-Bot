84d1c9bb19f59e8ef2fe7fab1e44d9f3c898366c
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-21 07:35:03 +0000

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -20,7 +20,7 @@ export default class Command extends BaseCommand {
 
 	run = async (M: ISimplifiedMessage): Promise<void> => {
 		const lisa =
-			"https://c.tenor.com/ix4745IOmb4AAAPo/blackpink-bp.mp4";
+			"https://c.tenor.com/kljjUE4XsnsAAAPo/quintessential-quintuplets-quintessential-quintuplets-s2.mp4";
 		return void this.client.sendMessage(
 			M.from,
 			{ url: lisa },
