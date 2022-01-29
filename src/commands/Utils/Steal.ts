e8e66269de68e60b83a27594f743c111409417f2
P-A-I-N <94308218+Pain77@users.noreply.github.com>
2022-01-29 22:29:53 +0530

    Update Steal.ts

--- src/commands/Utils/Steal.ts
+++ src/commands/Utils/Steal.ts
@@ -31,6 +31,7 @@ export default class Command extends BaseCommand {
 			description: "Will format the given sticker.",
 			category: "utils",
 			usage: `${client.config.prefix}steal[tag_sticker]|pack|author`,
+			adminOnly: true,
 			baseXp: 30,
 		});
 	}
